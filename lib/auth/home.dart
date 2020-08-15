2ac5ceebf2877302f043f7e7394f1243944b97b1
Nick Kress <kressnick25@gmail.com>
2020-08-15 19:01:04 +1000

    Fixed screens being wrapped with MaterialApp
    Only the main app wrapper should be wrapped with MaterialApp as this
    allows context and theming to be defined once and then inherited by all
    child widgets.

--- lib/auth/home.dart
+++ lib/auth/home.dart
@@ -2,65 +2,55 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   static const routeName = '/home';
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyPracticeApp',
-      theme: Theme.of(context),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Home"),
-        ),
-        body: HomePageOptions(),
-      ),
-    );
-  }
-}
 
-class HomePageOptions extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        Card(
-          child: ListTile(
-            title: Text(
-              'Most Recent',
-              style: const TextStyle(
-                fontSize: 22,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: Text(
+                'Most Recent',
+                style: const TextStyle(
+                  fontSize: 22,
+                ),
               ),
+              leading: Icon(Icons.storage),
+              trailing: Icon(Icons.keyboard_arrow_right),
             ),
-            leading: Icon(Icons.storage),
-            trailing: Icon(Icons.keyboard_arrow_right),
           ),
-        ),
-        Card(
-          child: ListTile(
-            title: Text(
-              'Settings',
-              style: const TextStyle(
-                fontSize: 22,
+          Card(
+            child: ListTile(
+              title: Text(
+                'Settings',
+                style: const TextStyle(
+                  fontSize: 22,
+                ),
               ),
+              leading: Icon(Icons.settings),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () => Navigator.pushNamed(context, '/settings'),
             ),
-            leading: Icon(Icons.settings),
-            trailing: Icon(Icons.keyboard_arrow_right),
-            onTap: () => Navigator.of(context).pushNamed('/settings'),
           ),
-        ),
-        Card(
-          child: ListTile(
-            title: Text(
-              'Logout',
-              style: const TextStyle(
-                fontSize: 22,
+          Card(
+            child: ListTile(
+              title: Text(
+                'Logout',
+                style: const TextStyle(
+                  fontSize: 22,
+                ),
               ),
+              leading: Icon(Icons.eject),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () => Navigator.popAndPushNamed(context, '/'),
             ),
-            leading: Icon(Icons.eject),
-            trailing: Icon(Icons.keyboard_arrow_right),
-            onTap: () => Navigator.of(context).popAndPushNamed('/'),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
