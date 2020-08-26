6933027f694823ee50ba3d05eee2d218c8e11704
Nick Kress <kressnick25@gmail.com>
2020-08-26 19:45:23 +1000

    subtly break the build

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,10 @@ void main() {
 
 class MyApp extends StatelessWidget {
   static Pointer<IntPtr> udContext = allocate();
-  static invalidCode;
+
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget builds(BuildContext context) {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
